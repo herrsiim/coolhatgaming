9524da646bb6c07ac2b752f393875f255c2d4adc
Herrsiim <herrsiim@gmail.com>
2020-09-17 09:35:02 +0300

    Created nav-bar comp.

--- src/app/views/home/home.module.ts
+++ src/app/views/home/home.module.ts
@@ -3,12 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { SharedModule } from 'src/app/components/shared.module';
+
 
 
 @NgModule({
   declarations: [HomeComponent],
   imports: [
     CommonModule,
+    SharedModule,
     HomeRoutingModule
   ]
 })
